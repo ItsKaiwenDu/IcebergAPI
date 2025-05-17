ecb1158ae18a4d3e7b57744de07ce29a3bef1c2b
ItsKaiwenDu <TheKaiwenDu@gmail.com>
2025-05-17 10:14:51 -0700

    Commit: Tailwind Integration

--- IcebergAPI/vite.config.js
+++ IcebergAPI/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss({
+      // optional: tell Tailwind where to look for classes
+      content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    }),
+  ],
 })
